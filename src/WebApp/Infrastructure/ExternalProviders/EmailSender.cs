557e977ae9abfd1f4d88fadcada55167b3ce0ba6
Nathan Khomych <nathankhomych@leobit.com>
2025-09-22 22:38:59 +0300

    some updates

--- src/WebApp/Infrastructure/ExternalProviders/EmailSender.cs
+++ src/WebApp/Infrastructure/ExternalProviders/EmailSender.cs
@@ -1,16 +1,20 @@
 ﻿using Domain.Interfaces;
 using Domain.Primitives;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Net;
 using System.Net.Mail;
+using System.Text.Json;
 
 namespace Infrastructure.ExternalProviders;
 internal class EmailSender : IEmailSender
 {
     private readonly SmtpSettings _smtpOptions;
-    public EmailSender(IOptions<SmtpSettings> smtpOptions)
+    private readonly ILogger _logger;
+    public EmailSender(IOptions<SmtpSettings> smtpOptions, ILogger<EmailSender> logger)
     {
         _smtpOptions = smtpOptions.Value;
+        _logger = logger;
     }
     public async Task SendAsync(string to, string subject, string body, string? from = null, CancellationToken cancellationToken = default)
     {
@@ -25,6 +29,12 @@ internal class EmailSender : IEmailSender
             emailClient.Credentials = new NetworkCredential(
                 _smtpOptions.User,
                 _smtpOptions.Password);
+
+            _logger.LogInformation($"SmtpSetting {JsonSerializer.Serialize(_smtpOptions)}");
+
+            emailClient.EnableSsl = true;
+
+            emailClient.Timeout = 5000;
 
             await emailClient.SendMailAsync(message, cancellationToken);
         }
